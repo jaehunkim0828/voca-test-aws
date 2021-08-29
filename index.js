8f659d896bbca0e9ff7ce7226d3b27c41e7ce8be
Jaehun Kim <kkaa81@naver.com>
2021-08-29 18:30:43 +0900

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+const port = 80;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('build'));
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*")
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('hello');
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
