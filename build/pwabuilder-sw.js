9e70a4fdbef7face21bdb8b5a7cd471d9d69461f
Jaehun Kim <kkaa81@naver.com>
2021-09-02 18:08:57 +0900

    fix(pwa): pwa builder-sw fix

--- build/pwabuilder-sw.js
+++ build/pwabuilder-sw.js
@@ -5,7 +5,7 @@ require('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const offlineFallbackPage = "offline.html";
 
 window.self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
