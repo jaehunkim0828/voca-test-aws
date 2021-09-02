97b5ebda6bbae6dde1de3e56aeebec2e4ab13287
Jaehun Kim <kkaa81@naver.com>
2021-09-02 17:48:17 +0900

    feat(pwa): create pwabuilder-sw and change favicon and image

---
+++ build/pwabuilder-sw.js
@@ -0,0 +1,47 @@
+// This is the "Offline page" service worker
+
+require('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+window.self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    window.self.skipWaiting();
+  }
+});
+
+window.self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (window.workbox.navigationPreload.isSupported()) {
+  window.workbox.navigationPreload.enable();
+}
+
+window.self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
